6ab24fba5c2c4d1bbad654f3e3c07d4c0a50caf6
MMillan <miguelmillandorado@gmail.com>
2020-05-16 01:35:01 -0500

    borrado de archivos sql dentro de src/data, se van a src/sql

--- src/data/clean_tables.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS trabajo.nodos;
-CREATE TABLE IF NOT EXISTS trabajo.nodos AS(
-  SELECT
-  substring(id_nodo  from '[^.]*')::int as nodo,
-  lat, lon
-  FROM  trabajo.nodos_aux
-);
-
-DROP TABLE IF EXISTS trabajo.nodos_aux;
