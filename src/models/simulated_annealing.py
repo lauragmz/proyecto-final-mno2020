5eb7b03d6125a233968b01f2a7b325b9d270db04
Marco <ronmoy007@hotmail.com>
2020-05-19 15:56:26 -0500

    Ajustes para integración en el GridSearch y proceso en paralelo

--- src/models/simulated_annealing.py
+++ src/models/simulated_annealing.py
@@ -26,7 +26,7 @@ class TravellingSalesmanProblem(Annealer):
         self.Tmin = sim_HiperParam.get('Tmin')
         self.steps = sim_HiperParam.get('steps')
         self.updates = sim_HiperParam.get('updates')
-             
+
     def move(self):
         """Funcion que intercambia dos nodos en la ruta con la intención de probar otras rutas"""
         initial_energy = self.energy()
@@ -45,41 +45,44 @@ class TravellingSalesmanProblem(Annealer):
         return e
 
 class SimulatedAnnealing():
-    
+
     def __init__(self, df_fv, dict_Hiper):
         """
-    Constructor de la clase.Se genera un diccionario de distancias entre nodos, el cual alimenta al 
+    Constructor de la clase.Se genera un diccionario de distancias entre nodos, el cual alimenta al
     algoritmo del viajero.
     Args:
         df_fv (dataFrame): DataFrame con la base de datos
         dic_Hiper(diccionario): Diccionario con hiperparametros.
     Returns:
-        dic(diccionario): Diccionario de distancias entre nodos. 
+        dic(diccionario): Diccionario de distancias entre nodos.
     """
-        nodos = df_fv.nodoA.unique().tolist()
+        nodos = df_fv.id_origen.unique().tolist()
+        for i in range(0,len(nodos)):
+            nodos[i]=int(nodos[i])
         combinaciones = [[a, b] for a in nodos for b in nodos]
-        df2 = pd.DataFrame(combinaciones,columns=['nodoA', 'nodoB'])
+        df2 = pd.DataFrame(combinaciones,columns=['id_origen', 'id_destino'])
         df2['distancia'] = 0
         i=0
         for row in df2.values:
             if (row[0]==row[1]):
                 row[2]= 0
             elif (row[0]!=row[1] and row[0]<row[1]):
-                v= df_fv.query('nodoA == {} & nodoB == {}'.format(row[0],row[1]))
+                v= df_fv.query('id_origen == {} & id_destino == {}'.format(row[0],row[1]))
                 v2=v['distancia'].tolist()
                 row[2]= v2[0]
             elif (row[0]!=row[1] and row[0]>row[1]):
-                v= df_fv.query('nodoA == {} & nodoB == {}'.format(row[1],row[0]))
+                v= df_fv.query('id_origen == {} & id_destino == {}'.format(row[1],row[0]))
                 v2= v['distancia'].tolist()
                 row[2]= v2[0]
             df2.iloc[i]=row
             i+=1
-        dic = {k : g.distancia.to_dict() for k, g in df2.set_index('nodoA').groupby('nodoB')}
+        dic = {k : g.distancia.to_dict() for k, g in df2.set_index('id_origen').groupby('id_destino')}
         random.shuffle(nodos)
         #Se llama a la clase TSP, encargada de aplicar simulated annealing al problema del viajero
         self.tsp = TravellingSalesmanProblem(nodos, dic, dict_Hiper)
-        return 
-    
+        self.str_Clave = 'SA'
+        return
+
     def Ejecutar(self):
         """Funcion que ejecuta el algoritmo de SA para TSP"""
         tm_inicio = time.time()
@@ -88,6 +91,6 @@ class SimulatedAnnealing():
         state, e = self.tsp.anneal()
         tm_final = time.time()
         self.nbr_MejorCosto = e
-        self.nbr_MejorCamino = state
-        self.nbr_TiempoEjec = tm_final - tm_inicio 
+        self.lst_MejorCamino = state
+        self.nbr_TiempoEjec = tm_final - tm_inicio
         return
